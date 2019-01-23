19efe02f8c8884b4b2b87dcbdc5780c8c7f4fadf
WangHanHong <35711983+WangHanHong@users.noreply.github.com>
2019-01-23 16:59:22 +0800

    page

---
+++ src/main/java/com/dx/tmall/util/Page4Navigate.java
@@ -0,0 +1,13 @@
+package com.dx.tmall.util;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ * copyright:
+ * Company: 上海悟泰信息科技有限公司
+ *
+ * @author wanghanhong
+ * @since 2019/1/23 16:59
+ */
+public class Page4Navigate {
+}
